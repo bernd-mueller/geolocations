b7729fa4886115c77ac303bce13e46be65a83aba
Bernd Müller <Muellerb@zbmed.org>
2021-04-08 14:58:01 +0200

    changed calculation of whole map to simply use saved rda object of the map

--- leafleteu/server.R
+++ leafleteu/server.R
@@ -1,36 +1,5 @@
 shinyServer(function (input, output) {
   output$map <- renderLeaflet({
-    m <- leaflet(europe) %>% clearBounds()
-    
-    bins <- c(0, 500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, Inf)
-    pal <- colorBin("YlOrRd", domain = europe$density, bins = bins)
-    
-    labels <- sprintf(
-      "<strong>%s</strong><br/>%g Bestellungen",
-      europe$name, europe$density
-    ) %>% lapply(htmltools::HTML)
-    
-    m <- m %>% addPolygons(
-      fillColor = ~pal(density),
-      weight = 2,
-      opacity = 1,
-      color = "white",
-      dashArray = "3",
-      fillOpacity = 0.7,
-      highlight = highlightOptions(
-        weight = 5,
-        color = "#666",
-        dashArray = "",
-        fillOpacity = 0.7,
-        bringToFront = TRUE),
-      label = labels,
-      labelOptions = labelOptions(
-        style = list("font-weight" = "normal", padding = "3px 8px"),
-        textsize = "15px",
-        direction = "auto")) %>% 
-      addLegend(pal = pal, values = ~density, opacity = 0.7, title = NULL,
-                position = "bottomright")  %>%
-      setView(20, 56, 2.5)
-    m
+    europe
   })
 })
